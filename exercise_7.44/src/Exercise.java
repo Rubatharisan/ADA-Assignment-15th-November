28a50c5b2863bea9ece01d9a6fd9bee206869e8f
Rubatharisan Thirumathyam <rt@morningtrain.dk>
2016-11-20 15:25:19 +0100

    Commentated exercise 7.44
    It should be done now - please test it later - before handing it in.

--- exercise_7.44/src/Exercise.java
+++ exercise_7.44/src/Exercise.java
@@ -10,57 +10,75 @@ public class Exercise {
 
 
     public static void main(String args[]){
-        System.out.println("Hello!");
 
+        // Create an instance of our exercise class
         Exercise myBooleanArray = new Exercise();
 
+
+        // Create a random boolean array with elements: true and false.
         myBooleanArray.makeBooleanArray(10);
 
+        // Lets print our boolean array.
         myBooleanArray.printArray();
 
+        // Lets sort our array - so false elements comes first.
         myBooleanArray.sort();
 
+        // Lets print our sorted array
         myBooleanArray.printArray();
-
-
-
 
     }
 
     private boolean[] makeBooleanArray(int size){
+
+        // Lets make a random object by Java.
         SecureRandom random = new SecureRandom();
+
+        // Define our myBooleanArray with a boolean array and size.
         myBooleanArray = new boolean[size];
 
+        // Lets make a for loop that puts our false / true values
         for (int i = 0; i < size; i++){
+
+            // If our next random integer is 0, then put false into the current iteration
             if( random.nextInt(2) == 0 ){
                 myBooleanArray[i] = false;
             } else {
+                // Otherwise put true in.
                 myBooleanArray[i] = true;
             }
         }
 
+        // Return our filled up boolean array
         return myBooleanArray;
 
     }
 
     private void printArray(){
+
+        // Lets print our boolean array
         System.out.println(Arrays.toString(myBooleanArray));
+
     }
 
     private void sort(){
 
+        // Lets count how many false elements there is in our array.
         int falseCounter = 0;
 
+        // For loop - If we catch any false elements, lets increment our falseCounter.
         for (int i = 0; i < myBooleanArray.length; i++){
             if(myBooleanArray[i] == false){
                 falseCounter++;
             }
         }
 
+        // For loop - lets create all first elements until [i] is above falseCounter as false.
         for (int i = 0; i < myBooleanArray.length; i++){
             if(i < falseCounter){
                 myBooleanArray[i] = false;
             } else {
+                // The rest will be true elements. 
                 myBooleanArray[i] = true;
             }
         }
